488a07abed1b4788847c2d54c8c65865718e93dc
Sergey Telshevsky <s.telshevsky@eco-fin.eu>
2021-03-29 07:50:00 +0300

    Add container adapter for PSR-11 support

---
+++ src/ContainerAdapter.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Amp\Injector;
+
+use Amp\Injector\Injector;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+class ContainerAdapter
+{
+    protected array $has = [];
+
+    public function __construct(protected Injector $injector) { }
+
+    public function get(string $id) : mixed
+    {
+        if (!$this->has($id)) {
+            throw new class(
+                'Unable to get: ' . $id
+            ) extends \InvalidArgumentException implements NotFoundExceptionInterface {};
+        }
+
+        try {
+            return $this->injector->make($id);
+        } catch (\Exception $previous) {
+            throw new class(
+                'Unable to get: ' . $id,
+                0,
+                $previous
+            ) extends \RuntimeException implements ContainerExceptionInterface {};
+        }
+    }
+
+    public function has(string $id) : bool
+    {
+        static $filter = Injector::I_BINDINGS
+            | Injector::I_DELEGATES
+            | Injector::I_PREPARES
+            | Injector::I_ALIASES
+            | Injector::I_SHARES;
+
+        if (isset($this->has[$id])) {
+            return $this->has[$id];
+        }
+
+        $definitions = array_filter($this->injector->inspect($id, $filter));
+        if (!empty($definitions)) {
+            return $this->has[$id] = true;
+        }
+
+        if (!class_exists($id)) {
+            return $this->has[$id] = false;
+        }
+
+        $reflector = new \ReflectionClass($id);
+        if ($reflector->isInstantiable()) {
+            return $this->has[$id] = true;
+        }
+
+        return $this->has[$id] = false;
+    }
+}
